59e2a83b7decb72e3d80a5dac6ba4f0d677691ae
carmenchanmy <67233182+carmenchanmy@users.noreply.github.com>
2025-05-21 20:26:55 +0100

    Update raw_patients_transformed.sql

--- code/raw_patients_transformed.sql
+++ code/raw_patients_transformed.sql
@@ -9,7 +9,8 @@ with raw_patients as (
   JSON_VALUE(data, '$.contact.email') AS email,
   JSON_VALUE(data, '$.contact.phone') AS phone
 FROM {{ ref('raw_pats') }}
-)
+),
+data_transform as (
 select
 safe_cast(patient_id as int64) as patient_id,
 safe_cast(practice_id as int64) as practice_id,
@@ -20,3 +21,9 @@ conditions,
 email,
 phone
 from raw_patients
+)
+
+select 
+    concat(patient_id,practice_id,email) as unique_patient_id,
+    *
+from data_transform
