a088f11ab35e19e99c8e01d1f5acff663f7ce337
carmenchanmy <67233182+carmenchanmy@users.noreply.github.com>
2025-05-21 21:21:07 +0100

    Update patients_practice_pcn.sql

--- code/patients_practice_pcn.sql
+++ code/patients_practice_pcn.sql
@@ -1,11 +1,22 @@
-select 
+with dupes as (
+  
+  select patient_id,count(*)
+  from {{ ref('raw_patients_transformed') }}
+  group by 1
+  having count(*) > 1
+)
+    
+  select 
   pt.*,
   pc.practice_name,
   pc.pcn,
-  pcn.pcn_name
+  pcn.pcn_name,
+  if(d.patient_id is null,true,false) as valid_patient_id_flag
 from {{ ref('raw_patients_transformed') }} pt
 inner join {{ ref('raw_practices') }} pc
 on pt.practice_id = pc.id
 inner join {{ ref('raw_pcns') }} pcn
 on pc.pcn = pcn.id
+left outer join dupes d
+on pt.patient_id = d.patient_id
 where pc.id <> 999
